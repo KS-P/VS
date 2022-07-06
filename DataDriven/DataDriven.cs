cc512ccef1bad17ee2b9376eeb46a1d62a7ec164
sivaranjani <ksp2721@gmail.com>
2022-07-06 10:48:22 -0500

    second commit

---
+++ DataDriven/DataDriven.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCSharpPractice
+{
+    [TestClass]
+    public class DataDriven
+    {
+
+        [Test]
+        [TestCase("Sun", "Day", "7/3/2022")]
+        [TestCase("Mon", "Day", "7/4/2022")]
+        [TestCase("Tues", "Day", "7/5/2022")]
+
+        public void DataDrivenTestingUsingDataRow(string fName, string lName, string eDate)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.uitestpractice.com/Students/Create");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(2);
+            driver.FindElement(By.Id("FirstName")).SendKeys(fName);
+            driver.FindElement(By.Id("LastName")).SendKeys(lName);
+            driver.FindElement(By.XPath("//input[@type='datetime']")).SendKeys(eDate);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+            driver.Quit();
+
+        }
+
+    }
+}
